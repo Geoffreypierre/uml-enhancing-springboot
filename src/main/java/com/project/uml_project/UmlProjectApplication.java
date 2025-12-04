e48abf8132fa10ca29dc5375b837ee25b26b9e82
Geoffreypierre <geoffrey.pierre@etu.umontpellier.fr>
2025-12-04 17:33:38 +0100

    first commit

---
+++ src/main/java/com/project/uml_project/UmlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.uml_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/uml_project/UmlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.uml_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
